1fd74d4f763981c16d5dbb49fbe2e0fc19d767ca
Yoran Brondsema <yoran@sutori.com>
2017-04-26 19:10:10 -0700

    Add benchmarks for more sizes

--- benchmark/browser.js
+++ benchmark/browser.js
@@ -2,42 +2,97 @@
 /* eslint-disable strict, no-console */
 
 $(function () {
-  var sample = {
-    width:  3200,
-    height: 2500
-  };
-  sample.buffer    = new Uint8Array(sample.width * sample.height * 4);
-
-  var p = window.pica({ features: [ 'js' ] });
-
-  /* eslint-disable new-cap */
-  Benchmark.Suite()
-
-  .add(`Resize of ${sample.width}x${sample.height}`, {
-    defer: true,
-    fn: function (defer) {
-      p.resizeBuffer({
-        src:      sample.buffer,
-        width:    sample.width,
-        height:   sample.height,
-        toWidth:  (sample.width * 0.15)|0,
-        toHeight: (sample.height * 0.15)|0,
-        quality:  3
-      })
-      .then(() => defer.resolve());
+  var samples = [
+    {
+      width: 3200,
+      height: 2500
+    },
+    {
+      width: 800,
+      height: 800
+    },
+    {
+      width: 50,
+      height: 50
+    },
+    {
+      width: 1,
+      height: 1
     }
-  })
+  ];
 
-  .add(`Unsharp of ${sample.width}x${sample.height}`, {
-    fn: function () {
-      p.__mathlib.unsharp(
-        sample.buffer, sample.width, sample.height,
-        80, 0.5, 4
-      );
-    }
-  })
+  var js = window.pica({ features: [ 'js' ] });
+  var ww = window.pica({ features: [ 'ww' ] });
+  var wasm = window.pica({ features: [ 'wasm' ] });
+  var cib = window.pica({ features: [ 'js', 'cib' ] });
 
-  .on('cycle', event => {
+  var suite = new Benchmark.Suite;
+
+  samples.forEach(function(sample) {
+    sample.buffer = new Uint8Array(sample.width * sample.height * 4);
+
+    suite.add(`Resize of ${sample.width}x${sample.height} (JS)`, {
+      defer: true,
+      fn: function (defer) {
+        js.resizeBuffer({
+          src:      sample.buffer,
+          width:    sample.width,
+          height:   sample.height,
+          toWidth:  (sample.width * 0.15)|0,
+          toHeight: (sample.height * 0.15)|0,
+          quality:  3
+        })
+          .then(() => defer.resolve());
+      }
+    })
+
+    .add(`Resize of ${sample.width}x${sample.height} (WW)`, {
+      defer: true,
+      fn: function (defer) {
+        ww.resizeBuffer({
+          src:      sample.buffer,
+          width:    sample.width,
+          height:   sample.height,
+          toWidth:  (sample.width * 0.15)|0,
+          toHeight: (sample.height * 0.15)|0,
+          quality:  3
+        })
+          .then(() => defer.resolve());
+      }
+    })
+
+    .add(`Resize of ${sample.width}x${sample.height} (WASM)`, {
+      defer: true,
+      fn: function (defer) {
+        wasm.resizeBuffer({
+          src:      sample.buffer,
+          width:    sample.width,
+          height:   sample.height,
+          toWidth:  (sample.width * 0.15)|0,
+          toHeight: (sample.height * 0.15)|0,
+          quality:  3
+        })
+          .then(() => defer.resolve());
+      }
+    })
+
+    .add(`Resize of ${sample.width}x${sample.height} (CIB)`, {
+      defer: true,
+      fn: function (defer) {
+        cib.resizeBuffer({
+          src:      sample.buffer,
+          width:    sample.width,
+          height:   sample.height,
+          toWidth:  (sample.width * 0.15)|0,
+          toHeight: (sample.height * 0.15)|0,
+          quality:  3
+        })
+          .then(() => defer.resolve());
+      }
+    });
+  });
+
+  suite.on('cycle', event => {
     var $el = $('<p></p>');
 
     $el.text(`> ${event.target}`);
@@ -45,7 +100,5 @@ $(function () {
 
     console.log(`> ${event.target}`);
   })
-
   .run();
-
 });
